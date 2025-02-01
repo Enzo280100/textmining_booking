212a3bb9e70e18b554f46a058f2c6c46881b9828
Enzo Infantes <enzo.infantes@bse.eu>
2025-02-01 19:37:38 +0100

    New version of the project

---
+++ booking/packages/selenium_setup.py
@@ -0,0 +1,92 @@
+# textmining_booking/utils.py
+import os
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, StaleElementReferenceException
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def ffx_preferences(dfolder, download=False):
+    '''
+    Sets the preferences of the firefox browser: download path.
+    '''
+    profile = webdriver.FirefoxProfile()
+    # set download folder:
+    profile.set_preference("browser.download.dir", dfolder) # you can predefine where you wanna store things in case its needed
+    profile.set_preference("browser.download.folderList", 2) # 0 means to download to the desktop, 1 means to download to the default "Downloads" directory, 2 means to use the directory
+    profile.set_preference("browser.download.manager.showWhenStarting", False) #I dont wanna see a pop up for each download so i swith it off
+    profile.set_preference("browser.helperApps.neverAsk.saveToDisk",
+                           "application/msword,application/rtf, application/csv,text/csv,image/png ,image/jpeg, application/pdf, text/html,text/plain,application/octet-stream")
+
+    # profile.install_addon('/Users/luisignaciomenendezgarcia/Dropbox/CLASSES/class_bse_text_mining/class_scraping_bse_2025/booking/booking/ublock_origin-1.55.0.xpi')
+    # profile.add_extension('/Users/luisignaciomenendezgarcia/Dropbox/CLASSES/class_bse_text_mining/class_scraping_bse/booking/booking/ublock_origin-1.55.0.xpi')
+
+
+    # this allows to download pdfs automatically
+    if download:
+        profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf,application/x-pdf")
+        profile.set_preference("pdfjs.disabled", True) #dont want the pdf viewer to open
+
+
+    options = Options()
+    options.profile = profile
+    options.binary_location = r"C:\Program Files\Mozilla Firefox\firefox.exe" #path to firefox (change it to your path)
+
+    return options
+
+
+def start_up(link, dfolder, geko_path,donwload=True):
+    # geko_path='/Users/luisignaciomenendezgarcia/Dropbox/CLASSES/class_bse_text_mining/class_scraping_bse/booking/geckodriver'
+    # download_path='./downloads'
+    os.makedirs(dfolder, exist_ok=True)
+
+    options = ffx_preferences(dfolder,donwload)
+    service = Service(geko_path)
+    browser = webdriver.Firefox(service=service, options=options)
+    # Enter the website address here
+    browser.get(link)
+    time.sleep(5)  # Adjust sleep time as needed
+    return browser
+
+
+def check_and_click(browser, xpath, type):
+    '''
+    Function that checks whether the object is clickable and, if so, clicks on
+    it. If not, waits one second and tries again.
+    '''
+    ck = False
+    ss = 0
+    while ck == False:
+        ck = check_obscures(browser, xpath, type)
+        time.sleep(1)
+        ss += 1
+        if ss == 15:
+            # warn_sound()
+            # return NoSuchElementException
+            ck = True
+            # browser.quit()
+
+def check_obscures(browser, xpath, type):
+    '''
+    Function that checks whether the object is being "obscured" by any element so
+    that it is not clickable. Important: if True, the object is going to be clicked!
+    '''
+    try:
+        if type == "xpath":
+            browser.find_element('xpath',xpath).click()
+        elif type == "id":
+            browser.find_element('id',xpath).click()
+        elif type == "css":
+            browser.find_element('css selector',xpath).click()
+        elif type == "class":
+            browser.find_element('class name',xpath).click()
+        elif type == "link":
+            browser.find_element('link text',xpath).click()
+    except (ElementClickInterceptedException, NoSuchElementException, StaleElementReferenceException) as e:
+        print(e)
+        return False
+    return True
